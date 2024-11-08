751327e7f985096da04d440fc21f664f7258e8aa
PeterCavv <pedroabad52@gmail.com>
2024-11-08 15:57:33 +0100

    Fin paso 13

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\CategoryController;
+use App\Http\Controllers\ProductController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -8,11 +9,11 @@ Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 
-Route::get('categories', [CategoryController::class, 'index']);
-Route::get('categories/{category}', [CategoryController::class, 'show']);
 Route::get('lists/categories', [CategoryController::class, 'list']);
-Route::post('categories', [CategoryController::class, 'store']);
-Route::delete('categories/{category}', [CategoryController::class, 'destroy']);
+
+Route::middleware('auth:sanctum')->group(function () {
+    Route::apiResource('categories', CategoryController::class);
+});
 
 Route::get('products', [ProductController::class, 'index']);
 
